33d655f38b5719120288284c95e01785f144a8f6
ruiyangio <ruiyangwind@gmail.com>
2018-07-17 16:03:53 -0700

    Add more

--- src/App.js
+++ src/App.js
@@ -12,8 +12,12 @@ const MATHJAX_CDN_URL =
 
 const regression = 'y~~f(X,beta)';
 const conditionalExpectation = 'E(Y|X) = f(X,beta)';
-const logitModel = 'h_theta(x) = g(theta^TX) = 1/(1+e^(-theta^TX))';
-const generalizedModel = 'E(Y|X)=mu=g^-1(Xbeta)';
+const logitModel = 'h_theta(x) = g(Xtheta) = 1/(1+e^(-Xtheta))';
+const linearRegression = 'y = Xbeta, yinRR';
+const generalizedModel = 'E(Y|X)=f(X,beta)=mu=g^-1(Xbeta)';
+const linkFunction = 'Xbeta = g(mu)';
+const beachExample = 'P(go|temperature) = p, P(notgo|temperature) = 1 - p';
+const logOdds = 'ln(p/(1-p)) = temperaturebeta';
 const sigmoidChartConfig = makeFunctionLineChart(
   -4.5,
   4.5,
@@ -100,28 +104,74 @@ class App extends Component {
           </section>
           <section>
             <h3>Generalized Linear Model</h3>
-            <div className="fragment" data-fragment-index="0">
-              <MathJax.Context input="ascii" script={MATHJAX_CDN_URL}>
-                <div>
-                  <MathJax.Node>{generalizedModel}</MathJax.Node>
-                </div>
-              </MathJax.Context>
-            </div>
-            <br />
-            <div className="fragment" data-fragment-index="1">
-              <div>
-                Y is from a particular distribution in the exponential family
+            <section>
+              <div className="fragment" data-fragment-index="0">
+                <MathJax.Context input="ascii" script={MATHJAX_CDN_URL}>
+                  <div>
+                    <MathJax.Node>{linearRegression}</MathJax.Node>
+                  </div>
+                </MathJax.Context>
               </div>
-            </div>
+              <br />
+              <div className="fragment" data-fragment-index="1">
+                <MathJax.Context input="ascii" script={MATHJAX_CDN_URL}>
+                  <div>
+                    <MathJax.Node>{generalizedModel}</MathJax.Node>
+                  </div>
+                </MathJax.Context>
+              </div>
+              <br />
+              <div className="fragment" data-fragment-index="2">
+                <MathJax.Context input="ascii" script={MATHJAX_CDN_URL}>
+                  <div>
+                    <MathJax.Node>{linkFunction}</MathJax.Node>
+                  </div>
+                </MathJax.Context>
+              </div>
+              <br />
+              <div className="fragment" data-fragment-index="3">
+                <div>
+                  Y is from a particular distribution in the exponential family
+                </div>
+              </div>
+            </section>
           </section>
           <section>
             <h3>Logistic Regression</h3>
             <section>
-              <MathJax.Context input="ascii" script={MATHJAX_CDN_URL}>
+              <div className="fragment" data-fragment-index="0">
+                Likelihood of a given person going to the beach at some
+                temperature
+              </div>
+              <br />
+              <div className="fragment" data-fragment-index="1">
+                <MathJax.Context input="ascii" script={MATHJAX_CDN_URL}>
+                  <div>
+                    <MathJax.Node>{beachExample}</MathJax.Node>
+                  </div>
+                </MathJax.Context>
+              </div>
+              <br />
+              <div className="fragment" data-fragment-index="2">
+                <MathJax.Context input="ascii" script={MATHJAX_CDN_URL}>
+                  <div>
+                    <MathJax.Node>{logOdds}</MathJax.Node>
+                  </div>
+                </MathJax.Context>
+                <br />
                 <div>
-                  <MathJax.Node>{logitModel}</MathJax.Node>
+                  a constant change in temperature leads to a constant change in
+                  odds
                 </div>
-              </MathJax.Context>
+              </div>
+              <br />
+              <div className="fragment" data-fragment-index="3">
+                <MathJax.Context input="ascii" script={MATHJAX_CDN_URL}>
+                  <div>
+                    <MathJax.Node>{logitModel}</MathJax.Node>
+                  </div>
+                </MathJax.Context>
+              </div>
             </section>
             <section>
               <h4>Sigmoid</h4>
